d593c184173713ac07028d6f8d857bcb106237eb
edith321 <edita.paulikaite@inbox.lt>
2017-07-11 12:59:56 +0300

    created arguments for job listing plugin
    also made labels translatable

--- wp-content/plugins/wp-job-listing/job-listing.php
+++ wp-content/plugins/wp-job-listing/job-listing.php
@@ -30,27 +30,57 @@ if(!defined('ABSPATH')) {
     exit;
 }
 function dwwp_register_post_type() {
-    $singular = 'Job Listing';
-    $plural = 'Job listings';
+    $singular = __('Job Listing');
+    $plural = __('Job listings');
 
     $labels = array(
         'name'                  => $plural,
         'singular_name'         => $singular,
-        'add_name'              => 'Add new',
-        'add_new_item'          => 'Add New ' . $singular,
-        'edit'                  => 'Edit',
-        'edit_item'             => 'Edit ' . $singular,
-        'new_item'              => 'New ' . $singular,
-        'view'                  => 'View ' . $singular,
-        'view_item'             => 'View ' . $singular,
-        'search_item'           => 'Search ' . $plural,
-        'parent'                => 'Parent ' . $singular,
-        'not_found'             => 'No ' . $plural . ' found',
-        'not_found_in_trash'    => 'No ' . $plural . ' in Trash'
+        'add_name'              => __('Add new'),
+        'add_new_item'          => __('Add New ') . $singular,
+        'edit'                  => __('Edit'),
+        'edit_item'             => __('Edit ') . $singular,
+        'new_item'              => __('New ') . $singular,
+        'view'                  => __('View ') . $singular,
+        'view_item'             => __('View ') . $singular,
+        'search_item'           => __('Search ') . $plural,
+        'parent'                => __('Parent ') . $singular,
+        'not_found'             => __('No ') . $plural . __(' found'),
+        'not_found_in_trash'    => __('No ') . $plural . __(' in Trash')
     );
     $args = array(
-        'labels' => $labels,
-    );
+        'labels'                => $labels,
+        'public'                => true, // koks turetu buti matomumas sito plugino
+        'publicly_queryable'    => true, // Whether queries for this post type can be performed from the front end.
+        'exclude_from_search'   => false, // shoud this post type be shown in search results
+        'show_in_nav_menus'     => true,  // will it be possible to add job listings to main menu
+        'show_ui'               => true, // if set to false, the tag dissappears from dashboard and you are able to create a custom one
+        'show_in_menu'          => true, //similar to show_ui
+        'show_in_admin_bar'     => true, // when set to true, it is visible ir the top admin menu on +new tag
+        'menu_position'         => 10, // controls where your post-type appears in the dashboard
+        'menu_icon'             => 'dashicons-businessman', // set the icon for the dashboard tag - dashicon (already included in wp)
+        // 'menu_icon' => get_stylesheet_directory_uri() . '/images/super-duper.png', --- custom menu item
+        'can_export'            => true, // do you want this post-type to be exportable
+        'delete_with_user'      => false, // true - when delete a user, its posts are also being deleted
+        'hierarchical'          => false, // true - acts like a page, false - acts like a post
+        'has_archive'           => true, // true - able use wp template for archive
+        'query_var'             => true, // allows to change url, true - just use the custom value
+        'capability_type'       => 'page', // who can have access to this custom post type, post - any user role have access, page - only admistrator and author
+        'map_meta_cap'          => true, //
+        // 'capabilities'       => array(), -- allows to create custom capabilities for user roles
+        'rewrite'               => array(
+            'slug'      => 'jobs', // sets permanent link .....lt/jobs/...
+            'with_front'=> 'true', // priesdelis, jei yra toks naudojamas
+            'pages'     => 'true', // easy pagination set up
+            'feeds'     => true, // whether you want it to be in rss feed
+        ),
+        'supports'               => array( // lists things you want to see in the post edit screen
+            'title',
+            'editor',
+            'author',
+            'custom-fields'
+        )
+        );
     register_post_type('job', $args);
 }
 add_action('init', 'dwwp_register_post_type');
